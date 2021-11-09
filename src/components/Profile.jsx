9ec21005a6a6c6dafb857a368aa21d5291a3e62a
Andrew <andrewhipsta@gmail.com>
2021-11-09 16:01:12 +0700

    starting-app

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className="content">
+			<img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" alt="forest" />
+			<div>
+				<img src="https://img.mfcimg.com/photos2/566/5661907/avatar.300x300.jpg?nc=1538677274" alt="photo" />
+				ava + desc
+			</div>
+			<div>
+				posts
+				<div>new post</div>
+				<div>
+					<div>post1</div>
+					<div>post1</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
